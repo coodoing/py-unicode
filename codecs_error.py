2144c352db7f34fd94bd4c034bce5363104bb290
xz <xiangzhi@lightinthebox.com>
2013-09-16 11:21:34 +0800

    codecs utf8，gbk codecs error

---
+++ codecs_error.py
@@ -0,0 +1,124 @@
+#-*-coding:utf-8-*-
+import re,sys,os
+import locale,codecs
+
+'''
+    open file use the default encoding
+    以默认的编码格式打开文件：没有使用binary格式
+'''
+def print_str(str):
+    print(str)
+
+def print_len(str):
+    str_len = len(str)
+    print(str_len)
+
+def codecs_encode(str,encoding='utf-8'):
+    return str.encode(encoding)
+    pass
+
+def codecs_decode(b_str,encoding='utf-8'):
+    if type(b_str) == str:
+        return
+    print(b_str.decode(encoding))
+    pass
+
+def print_file_info(filename):
+    data = open(filename).read()
+    print(data)
+    codecs_decode(data)
+    pass
+
+def print_file_line(filename):
+    with open(filename) as file:
+        for line in file:
+            print(line)    
+    pass
+
+def print_codecs_file_line(filename):
+    with codecs.open(filename) as file:
+        for line in file:
+            print(line)    
+    pass
+
+def print_file_line_encoding(filename,encoding = 'utf-8'):
+    with open(filename,encoding = 'utf-8') as file:
+        for line in file:
+            print(line)  
+    pass
+
+
+if __name__ == '__main__':
+    ch_str = '中文'
+    unicode_str = u'中文'
+    print('<strong>python中文:')
+    print_str(ch_str)
+    print_str(unicode_str)
+    print('<strong>python encode():')
+    print_str(codecs_encode(ch_str))
+    print_str(codecs_encode(ch_str,'gbk'))
+    print_str(codecs_encode(ch_str,'gb18030'))
+    print_str(codecs_encode('1ère'))
+    print_str(codecs_encode('1ère','gbk'))
+    print('<strong>python decode():')
+    codecs_decode(codecs_encode(ch_str))
+    codecs_decode(codecs_encode(unicode_str))
+    try:
+        codecs_decode(codecs_encode(ch_str,'gbk'))
+    except Exception:
+        print('<strong>utf-8 codec decode error')
+
+    codecs_decode(codecs_encode('1ère Recuérdame Ã©couteur Ã§a'))
+    codecs_decode(codecs_encode('1ère Recuérdame Ã©couteur Ã§a'),'gbk')
+    try:
+        codecs_decode(codecs_encode('1ère','gbk'))
+    except Exception:
+        print('<strong>utf-8 codec decode error')
+
+    code_str = '中国'
+    print(code_str.encode().decode())
+    print(code_str.encode().decode('mbcs','ignore'))
+    try:
+        print(code_str.encode().decode('gbk','strict'))
+    except Exception:
+        print('<strong>gbk codec decode error')
+    print('python长度：')
+    
+    '''
+    encoding = 'utf-8'
+    英语目录下，报错；法语目录下，没问题
+    否则, 恰好相反
+    '''
+    dir_path = ['languages/english/languages.php']
+    #dir_path = ['languages/english/languages.php','languages/france/languages.php']
+    for i in range(len(dir_path)):
+        print_file_line(dir_path[i])
+
+    dir_path = ['languages/france/languages.php']
+    for i in range(len(dir_path)):
+        print_file_line_encoding(dir_path[i])
+        #print_codecs_file_line(dir_path[i])
+
+    dir_path = ['languages/english/languages.php']
+    for i in range(len(dir_path)):
+        try:
+            print_file_line_encoding(dir_path[i])
+        except Exception:
+            print('<strong>english utf-8 error')
+        #print_codecs_file_line(dir_path[i])
+
+    dir_path = ['france.txt']
+    for i in range(len(dir_path)):
+        try:
+            print_file_line(dir_path[i])
+        except Exception:
+            print('<strong>france gbk error')
+        #print_codecs_file_line(dir_path[i])
+
+    dir_path = ['languages/english/languages.php','languages/france/languages.php']
+    file_output = ['lang/en/aaa.txt','lang/fr/aaa.txt']
+    for i in range(len(dir_path)):
+        try:
+            print_codecs_file_line(dir_path[i])
+        except Exception:
+            print('<strong>error')
--- codecs_method.py
+++ codecs_method.py
@@ -7,14 +7,9 @@ import locale,sys,codecs,os
     #open() function : has buffering option to use the buffering line  
 '''
 
-# codecs.open()打开文件
-def open_file_codecs(filename):
-    with codecs.open(filename,'rb') as file:
-        for line in file:
-            print(line)
-
-def open_file_text(filename):        
-    '''
+'''
+################################################################
+文件操作
     默认使用sys.getfilesystemencoding()编码格式打开文件，在window7中文环境中使用CP936（GBK）打开文件
     所以对于unicode,utf8,mscs的不同编码格式，encoding设置会出问题（utf8 gbk codecs error）
     对于多语言，这里最好别用 encoding = 'utf-8' encoding = 'utf-16_LE' 
@@ -23,7 +18,15 @@ def open_file_text(filename):
        2 利用rb，转换成b（unicode）形式，不过在写入的时候需要进行encode操作;（可行）
        3 把txt文件转换成UTF-8格式，然后在打开文件的时候指定编码格式utf-8(可行)
        所以最好的打开方式是以binary方式打开文件，转换成unicode
-    ''' 
+
+'''
+# codecs.open()打开文件
+def open_file_codecs(filename):
+    with codecs.open(filename,'rb') as file:
+        for line in file:
+            print(line)
+
+def open_file_text(filename):  
     with open(filename,'rb') as file:
         for line in file:
             print(line)
@@ -68,36 +71,92 @@ def write_file_append(filename,string):
 
         file.write(string.encode())
 
+'''
+################################################################
+常用的函数封装
+'''
+def print_str(str):
+    print(str)
+
+def print_len(str):
+    str_len = len(str)
+    print(str_len)
+
+def codecs_encode(str,encoding='utf-8'):
+    return str.encode(encoding)
+    pass
+
+def codecs_decode(b_str,encoding='utf-8'):
+    if type(b_str) == str:
+        return
+    print(b_str.decode(encoding))
+    pass
+
+def print_file_info(filename):
+    data = open(filename).read()
+    print(data)
+    codecs_decode(data)
+    pass
+
+def print_file_line(filename):
+    with open(filename) as file:
+        for line in file:
+            print(line)    
+    pass
+
+def print_file_line_encoding(filename,encoding = 'utf-8'):
+    with open(filename,encoding = 'utf-8') as file:
+        for line in file:
+            print(line)  
+    pass
+
+def print_codecs_file_line(filename):
+    with codecs.open(filename) as file:
+        for line in file:
+            print(line)    
+    pass
+'''
+################################################################
+'''
+
 if __name__ == '__main__':
+    print('################################################################')
     print('<strong>python系统参数：')
     print(locale.getdefaultlocale()) #('zh_CN', 'cp936')
     print(locale.getpreferredencoding()) # cp936
     print(sys.getdefaultencoding()) #utf-8
     print(sys.getfilesystemencoding())#mbcs
+    print(sys.maxunicode)
     print(codecs.lookup('utf-8'))#codeinfo class
 
+    print('################################################################')
     print('<strong>python长度：')    
-    print(len('含'))
-    print(len(u'含'))
-    
+    print(len('中文'))
+    print(len(u'中文'))
+    print_len('中文')
+    print_len('1234')
+    print_len('我爱中国')
+
+    print('################################################################')
     print('<strong>python中文编码：')
     code_str = u'中国'
     print(code_str.encode())
     print(code_str.encode('gbk'))
     print(code_str.encode().decode())
-    print(code_str.encode().decode('mbcs','ignore'))
-    #print(code_str.encode().decode('gbk','strict'))
+    print(code_str.encode().decode('mbcs','ignore'))#mbcs 乱码
+    #print(code_str.encode().decode('gbk','strict')) #gbk codecs error
 
-    print('<strong>python正常的编码和解码方式：')
     french = 'ry dialect: /a/, /ɑ/, /e/, /ɛ/, /ə/, /i/, /o/, /ɔ/, /y/, /u/, /œ/, /ø/, '
     print(french.encode())
-    print(french.encode().decode())    
-
+    print(french.encode().decode())
+    
+    print('################################################################')    
     path = 'encoding'
     french_unicode = path+'/unicode/'+'french-unicode.txt'
     french_utf8 = path+'/utf8/'+'french-utf8.txt'
     french_mscs = path+'/mscs/'+'french-mscs.txt'
     print('<strong>文件操作：')
+    print('########################')
     print('<strong>codecs.open方式打开文件：')
     open_file_codecs('french-unicode.txt')
     open_file_codecs('french-mscs.txt')
@@ -105,22 +164,31 @@ if __name__ == '__main__':
     open_file_codecs(path+'/open-test.txt')
     encode_decode_file(path+'/append-test.txt')
 
+    print('########################')
     print('<strong>open方式打开文件：')    
     open_file_text('french-unicode.txt')#需特别注意编码问题，在win7中文系统环境中，默认会提示gbk decode error
     open_file_text('french-mscs.txt')#默认的保存格式，会造成字符的丢失
     open_file_text('french-utf8.txt')#不会出问题，但需注意open的encoding格式，否则会出现乱码
 
+    print('########################')
     print('<strong>file encoding打开文件：')
     file_encoding('french-unicode.txt')
     file_encoding('french-mscs.txt')
     file_encoding('french-utf8.txt')
-    
+
+    print('########################')
     print('<strong>binary写文件：')
     write_file(path+'/write-test.txt')#以wb状态写文件，不管文件的格式，最终都正确写入文件，并将txt文件格式修改为UTF-8
     write_file_append(path+'/append-test.txt','başparmak')        
 
+    print('########################')
     print('<strong>文件遍历：')
     path = ['french-utf8.txt']#,'french_mscs.txt']#,'french.txt']
     for i in range(0,len(path)):
         open_file_text(path[i])
-    
+
+    print('########################')
+    codecs.BOM_UTF8.decode( "utf8" )
+    s = sys.stdin.read(3)
+    if s!=codecs.BOM_UTF8:
+        sys.stdout.write(s)
