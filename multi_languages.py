eb2421a06619dc3116ea4a52a8afafdcb9ad7763
coodoing <jairdxs@gmail.com>
2013-10-07 15:04:13 +0800

    clean

--- multi_languages.py
+++ multi_languages.py
@@ -1,5 +1,4 @@
 #-*-coding:utf-8-*-
-
 import re,sys,os
 import codecs,locale
 import xdrlib,xlrd
@@ -7,7 +6,6 @@ import getopt
 import glob
 from shutil import copytree, ignore_patterns
 from fdshutil import FileBak
-
 
 lang = {}
 def set_global_languages():
@@ -46,55 +44,7 @@ def set_global_languages():
 	lang["TH"]="thai"
 	lang["MS"]="malay" 
 
-
-class FileHandler(object):
-
-	def __init__(self):
-		pass
-
-	'''
-		只读模式打开文件
-        '''
-	def open_file_readable(self,filename):
-		pass
-
-	'''
-		写模式打开文件
-	'''
-	def open_file_writable(self,filename):
-
-		pass
-
-	def edit_file(self,filename,str,value):
-
-	 	with open(filename,"rb+") as file:
-	 		end = 0
-	 		lines = []
-	 		for line in file:
-	 			if not line:
-	 				break
-
-	 			elif line.find("?>".encode())!=-1:
-	 				end = 1
-	 				
-	 			elif line.find(value.encode())!=-1:
-	 				edit_str = "define('"+str+"', \""+value+"\")"
-	 				#print(edit_str)
-	 				lines.append(edit_str.encode())
-	 				
-
-	 			else:
-	 		 		lines.append(line)
-
-	 	with open(filename,'wb+') as file:
-	 		for line in lines:
-	 			file.write(line)
-	 		#file.write(str.encode())
-	 		file.write('\n'.encode())
-	 		if end:
-	 			file.write("?>".encode())
-
-	#@staticmethod
+class FileHandler(object):	
 	def write_file(self,filename,str):
 	 	with open(filename,"rb+") as file:
 	 		end = 0
@@ -102,13 +52,10 @@ class FileHandler(object):
 	 		for line in file:
 	 			if not line:
 	 				break
-
 	 			elif line.find("?>".encode())!=-1:
 	 				end = 1
-
 	 			else:
 	 		 		lines.append(line)
-
 	 	with open(filename,'wb+') as file:
 	 		for line in lines:
 	 			file.write(line)
@@ -116,10 +63,29 @@ class FileHandler(object):
 	 		file.write('\n'.encode())
 	 		if end:
 	 			file.write("?>".encode())
-
+				
+	def edit_file(self,filename,str,value):
+	 	with open(filename,"rb+") as file:
+	 		end = 0
+	 		lines = []
+	 		for line in file:
+	 			if not line:
+	 				break
+	 			elif line.find("?>".encode())!=-1:
+	 				end = 1	 				
+	 			elif line.find(value.encode())!=-1:
+	 				edit_str = "define('"+str+"', \""+value+"\")"
+	 				lines.append(edit_str.encode())
+	 			else:
+	 		 		lines.append(line)
+	 	with open(filename,'wb+') as file:
+	 		for line in lines:
+	 			file.write(line)
+	 		file.write('\n'.encode())
+	 		if end:
+	 			file.write("?>".encode())
 
 class LanguageHandler(object):
-
 	file_handler = FileHandler()
 	languages = {}	
 	tag = []
@@ -127,9 +93,7 @@ class LanguageHandler(object):
 	excel_name = ""
 	input_dir = ""
 	output_dir = ""
-	'''
-		language initialization
-	'''
+
 	def __init__(self,lang,tag,file_change,excel,output_dir):		
 		self.languages = lang
 		self.tag = tag
@@ -140,178 +104,110 @@ class LanguageHandler(object):
 	# @classmethod
 	# @staticmethod
 	# def __new__(clr, *args, **kwds):
-	# 	#cls.languages = lang
+	# 	cls.languages = lang
 	# 	pass
-		
-	'''
-		get languages
-	'''
+
 	def get_languages(self):
-		#print(self.languages)
 		return sef.languages	
 
-	'''
-		excel open 
-	'''
 	def open_excel(self):
 	    try:
 	        data = xlrd.open_workbook(self.excel_name)
-	        #print(data)
 	        return data
 	    except Exception:
-	        #print str(e)
 	        print("open excel error")
 
-	'''
-		append tag
-	'''
+	# 添加tag
 	def append_tags(self):
 	    data = self.open_excel()
 	    table = data.sheets()[0]
-	    nrows = table.nrows #行数
-	    ncols = table.ncols #列数
+	    nrows = table.nrows
+	    ncols = table.ncols
 	    
 	    language_output_files = []
 	    for j in range(0,ncols):
-		    lang_str = str(table.cell(0,j)) #获取头文件
+		    lang_str = str(table.cell(0,j))
 		    lang_str = lang_str.split("'")[1].split("\\")[0]
 		    language_output_file_name = self.output_dir + "/" + self.languages[lang_str]+"/"+self.file_change
 		    language_output_files.append(language_output_file_name)
 		    #language_output_files.append(lang_str)
-	    #print(language_output_files)   #output_files列表
-
 	    #new_tags = self.get_new_tags('/encoding/language-test.txt',self.tag)
-	    #print(new_tags)
-	    #print("class internal flags")
 
 	    tags_flags = []
 	    for i in range(1,nrows):
 	    	flags = self.has_tags(language_output_files[j],self.tag[i-1])
-	    	tags_flags.append(flags)
-            
+	    	tags_flags.append(flags)            
 
 	    #注意编码问题
 	    for i in range(1,nrows):
 	    	flags = 0
-	    	for j in range(0,ncols):	
-	    		#flags = 0
+	    	for j in range(0,ncols):
 		    	lang_str = table.cell(i,j).value.encode("utf-8")
-		    	#print(lang_str)
 		    	output_string = "define('"+self.tag[i-1]+"', \""+lang_str.decode("utf-8")+"\")"
 		    	output_string = output_string + "\n"
-		    	#print("append tag key:"+self.tag[i-1])
-		    	#print("append tag value:"+lang_str.decode("utf-8"))
-		    	#print("append tag:"+output_string)#print(type(output_string))
-		    	#print("output file path:"+language_output_files[j])		    	
-
 		    	try:
 		    		flags = self.has_tags(language_output_files[j],self.tag[i-1])
-		    		#print(flags)
 		    	except Exception:
 		    		print('open file error')
-		    	if  flags == 0:
-		    		#self.file_handler.write_file(language_output_files[j],output_string)
-		    		try:		    			
-		    			#output = "/encoding/language-test.txt"
-		    			#self.file_handler.write_file("encoding/language-test.txt",output_string)
-		    			self.file_handler.write_file(language_output_files[j],output_string)
-		    			
+		    	if  flags == 0:		    		
+		    		try:
+		    			self.file_handler.write_file(language_output_files[j],output_string)		    			
 		    		except Exception:
 		    			raise "write file error"
 		    	else:
 		    		print("Tag '"+self.tag[i-1]+"' is already in file=> "+language_output_files[j]+"!!!")
-
-		    	#for k in range(0,len(new_tags)):
-		    	   	#self.file_handler.write_file("encoding/language-test.txt",output_string)
 	    print("append successful!\n")
 	    return True
 
-	'''
-		edit tag
-	'''
+	# 编辑tag
 	def edit_tags(self,tag,value):
 	    data = self.open_excel()
 	    table = data.sheets()[0]
-	    nrows = table.nrows #行数
-	    ncols = table.ncols #列数
+	    nrows = table.nrows
+	    ncols = table.ncols
 	    
 	    language_output_files = []
 	    for j in range(0,ncols):
-		    lang_str = str(table.cell(0,j)) #获取头文件
+		    lang_str = str(table.cell(0,j))
 		    lang_str = lang_str.split("'")[1].split("\\")[0]
 		    language_output_file_name = self.output_dir + "/" + self.languages[lang_str]+"/"+self.file_change
 		    language_output_files.append(language_output_file_name)
         
-	    #注意编码问题
+	    #编码问题
 	    for j in range(0,ncols):
-	    	#flags = 0
-	    	#for j in range(0,ncols):	
-	    		lang_str = table.cell(1,j).value # true value
-		    	#print(lang_str)
-		    	#print("append tag key:"+self.tag[i-1])
-		    	#print("append tag value:"+lang_str.decode("utf-8"))
-		    	#print("append tag:"+output_string)#print(type(output_string))
-		    	#print("output file path:"+language_output_files[j])	    	 	
-
-		    	flags = self.has_tags(language_output_files[j],tag)
-		    	#print(flags)#= self.has_tags(language_output_files[j],self.tag[i-1])
-		    	
-		    	if  flags == 1:
-		    		
-		    		try:		    			
-		    			
-		    			self.file_handler.edit_file(language_output_files[j],tag,value)
-		    			print("Tag '"+tag+"' edit successful in file e=> "+language_output_files[j])#self.file_handler.edit_file(language_output_files[j],tag,value)  
-		    		except BaseException:
-		    			raise "edit file error"
-		    	else:
-		    		print("Tag '"+tag+"' must append to file=> "+language_output_files[j]+"!!!")
-
-		    	#for k in range(0,len(new_tags)):
-		    	   	#self.file_handler.write_file("encoding/language-test.txt",output_string)
+	    	lang_str = table.cell(1,j).value
+		    flags = self.has_tags(language_output_files[j],tag)		    	
+			if  flags == 1:				
+				try:					
+					self.file_handler.edit_file(language_output_files[j],tag,value)
+					print("Tag '"+tag+"' edit successful in file e=> "+language_output_files[j])#self.file_handler.edit_file(language_output_files[j],tag,value)  
+				except BaseException:
+					raise "edit file error"
+			else:
+				print("Tag '"+tag+"' must append to file=> "+language_output_files[j]+"!!!")
 	    print("edit successful!\n")
 	    return True
 
-	'''
-		判断文件中是否存在tag
-	'''
+	# 是否存在tag
 	def has_tags(self,filename,tag):
 		exsit = 0
-		#with open(filename,'r', encoding = 'utf-8') as file:
 		with open(filename,'rb') as file:
-			#print(file)
 			for line in file:
-				#print(line)
-				
-				#print(line)
-				# if not line:
-				# 	continue						
 				if line.find(tag.encode())!=-1:
-					#print("exsit")
 					exsit = 1
 					break
 				else:
 					continue
-			#print(tag)
-			#print(exsit)
-
-			return exsit
-		pass
-
-	'''
-		将有的tag进行剔除，获得新的需要添加的tag
-	'''
+			return exsit		
+	
+	# 在进行剔除，获得新的需要添加的tag	
 	def get_new_tags(self,filename,tags):
 		new_tags = []
 		print("length:%i",len(tags))
 		for i in range(0,len(tags)):
-			#print(tags[i])
 			exsit = 0
 			with open(filename,'r') as file:
 				for line in file:
-					#print(line)
-					#if not line:						
-						#break
 					if line.find(tags[i])!=-1:
 						exsit = 1
 						break
@@ -321,19 +217,7 @@ class LanguageHandler(object):
 				new_tags.append(tags[i])		
 		return new_tags
 
-	
-
-	pass
-
-#shutil的备份
-def bak_shutil():
-        
-        pass
-        
-
-'''
-	系统中的自定义变量
-'''
+#################################################################
 excel_name = "encoding/excel.xls" #打开的excel文件
 tags =['TXT_EMAIL']
 #tags = ["APPEND_NEW_TAGS_11","APPEND_NEW_TAGS_12"] #新添加的tag字段
@@ -344,20 +228,19 @@ bak_dir = 'encoding/tmp'
 if __name__ == '__main__':
 	print(sys.getdefaultencoding())
 	set_global_languages()
-
 	bak = FileBak(output_dir,bak_dir)
 	try:
 		bak.copy()
 	except WindowsError:
 		print('copy error')
-
 	lang_handler = LanguageHandler(lang,tags,file_change,excel_name,output_dir)	
+	lang_handler.edit_tags('TXT_EMAIL','TXT_EMAIL')
+	ret = lang_handler.append_tags()
+	if ret == False:
+		bak.rm()
+	
 	#print(lang_handler.has_tags('encoding/language-test.txt','APPEND_NEW_TAGS_32'))
 	#print(lang_handler.has_tags('encoding/language-test.txt','APPEND_NEW_TAGS_42'))
 	#print(lang_handler.get_new_tags('encoding/language-test.txt',tags))
 	#lang_handler.get_languages()
 	#lang_handler.open_excel()
-	lang_handler.edit_tags('TXT_EMAIL','TXT_EMAIL')
-	ret = lang_handler.append_tags()
-	if ret == False:
-		bak.rm()
